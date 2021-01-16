883e312b528cebb935b0469ad98efca6dff69fcf
Ricardo Rodrigues <r.jorge.rodrigues@hotmail.com>
2021-01-16 11:52:22 +0000

    Add project files.

---
+++ Calculadora/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Globalization;
+
+namespace Calculadora
+{
+    class Program
+    {
+        static double Pi = 3.14;
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("Insira o valor do raio: ");
+            double raio = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+
+            double circ = Circunferencia(raio);
+            Console.WriteLine("Circunferencia: " + circ.ToString("F2", CultureInfo.InvariantCulture));
+            double volume = Volume(raio);
+            Console.WriteLine("Volume: " + volume.ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine("O valor de Pi é: "+ Pi.ToString("F2", CultureInfo.InvariantCulture ));
+
+        }
+        static double Circunferencia(double r)
+        {
+            return 2.0 * Pi * r;
+        }
+        static double Volume(double r)
+        {
+            return 4.0 / 3.0 * Pi * Math.Pow(r, 3.0) ;
+        }
+    }
+}
